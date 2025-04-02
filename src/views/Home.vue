52c5f6e32f2a863db4cb400eb1ca4c359b5056f3
Koutarou Ushimaru <uxsxmxr14@gmail.com>
2025-04-02 10:42:51 +0900

    課題7:グループ管理機能の実装

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,22 @@
 <template>
-  <div>
-    <ContactList v-if="!showForm" @show-form="handleShowForm" />
+  <div class="container mt-4">
+    <div v-if="!showContactForm && !showGroupForm">
+      <ContactList @show-form="handleShowContactForm" />
+      <GroupList @show-group-form="handleShowGroupForm" />
+    </div>
+
     <ContactForm
-      v-else
+      v-if="showContactForm"
       :mode="formMode"
       :contact="selectedContact"
-      @form-closed="showForm = false"
+      @form-closed="showContactForm = false"
+    />
+
+    <GroupForm
+      v-if="showGroupForm"
+      :mode="groupFormMode"
+      :group="selectedGroup"
+      @form-closed="showGroupForm = false"
     />
   </div>
 </template>
@@ -13,25 +24,39 @@
 <script>
 import ContactList from '@/components/ContactList.vue'
 import ContactForm from '@/components/ContactForm.vue'
+import GroupList from '@/components/GroupList.vue'
+import GroupForm from '@/components/GroupForm.vue'
 
 export default {
   name: 'HomeView',
   components: {
     ContactList,
-    ContactForm
+    ContactForm,
+    GroupList,
+    GroupForm
   },
   data() {
     return {
-      showForm: false,
+      showContactForm: false,
       formMode: 'add',
-      selectedContact: null
+      selectedContact: null,
+      showGroupForm: false,
+      groupFormMode: 'add',
+      selectedGroup: null
     }
   },
   methods: {
-    handleShowForm({ mode, contact = null }) {
+    handleShowContactForm({ mode, contact = null }) {
       this.formMode = mode
       this.selectedContact = contact
-      this.showForm = true
+      this.showContactForm = true
+      this.showGroupForm = false
+    },
+    handleShowGroupForm({ mode, group = null }) {
+      this.groupFormMode = mode
+      this.selectedGroup = group
+      this.showGroupForm = true
+      this.showContactForm = false
     }
   }
 }
